a64b470bf7d84f3da31f73b30e1818ee6c5899fc
annderevyankina <anka73der@gmail.com>
2025-04-09 00:35:44 +0300

    Initial Commit

---
+++ Eflectica/EflecticaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EflecticaApp.swift
+//  Eflectica
+//
+//  Created by Анна on 09.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct EflecticaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
